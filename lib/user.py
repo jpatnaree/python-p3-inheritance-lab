76c8f5ddcdfd3bbcff79d7ca207c3533dfa87c1f
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-11-15 14:31:29 -0500

    lab

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    def __init__(self, first_name, last_name,):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __repr__(self):
+        print(f'{self.first_name} {self.last_name}')
